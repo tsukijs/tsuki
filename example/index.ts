21782bfc8d2014bd1bcf49f0b448bc83b97b9311
Saif Ali <this.saif.email@gmail.com>
2025-05-13 08:51:25 +0530

    tested text(), json() response method

--- example/index.ts
+++ example/index.ts
@@ -2,5 +2,7 @@ import { Tsuki } from "../src/core/tsuki";
 
 
 const app = new Tsuki();
-app.get('/hello', (ctx)=> new Response('Hello World!'));
+app.get('/hello', (c)=> new Response('Hello World!'));
+app.get('/json', (c)=> c.json({message: 'Hello World!'}));
+app.get('/text', (c)=> c.text('this is a text response'));
 app.serve(3000);
