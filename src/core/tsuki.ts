22c513f366b71791d5e4da4f7bc7369ea5918998
Saif Ali <this.saif.email@gmail.com>
2025-05-13 09:40:29 +0530

    separated creating Conext object f

--- src/core/tsuki.ts
+++ src/core/tsuki.ts
@@ -1,48 +1,57 @@
-import type { HttpMethodType } from "../types";
-import type { Context } from "./context";
-import { Router, type RouteHandler } from "./router";
+import type { HttpMethodType } from '../types';
+import type { Context } from './context';
+import { Router, type RouteHandler } from './router';
 
 export class Tsuki {
-    private router: Router = new Router();
+  private router: Router = new Router();
 
-    get(path:string, handler: RouteHandler) {
-        this.router.addRoute('GET', path, handler);
-        return this;
-    }
+  private createContext(req: Request): Context {
+    const text = (body: string, status = 200) => {
+      return new Response(body, {
+        status,
+        headers: { 'Content-Type': 'text/plain' },
+      });
+    };
 
-    //Running server
-    serve(port: number) {
-        
-        //Bun server
-        Bun.serve({
-            port,
-            fetch: (req)=> {
+    const json = <R>(body: R, status = 200) => {
+      return new Response(JSON.stringify(body), {
+        status,
+        headers: { 'Content-Type': 'application/json' },
+      });
+    };
+    const ctx: Context = { req, params: {}, body: null, text, json };
 
-                const url = new URL(req.url);
-                const route = this.router.findRoute(req.method as HttpMethodType, url.pathname);
-                console.log("reached in serve: ", route);
+    return ctx;
+  }
 
-                //If router is found then
-                if(route) {
-                    const ctx: Context = {
-                        req, 
-                        params: {}, 
-                        body: null,
-                        text: (body, status = 200) => {
-                            return new Response(body, {status, headers: {'Content-Type': 'text/plain'}});
-                            },
-                        json: (body, status = 200) => {
-                            return new Response(JSON.stringify(body), {status, headers: {'Content-Type': 'application/json'}});
-                            }
-                    };
-        
-                    return route.handler(ctx);
-                }
-                return new Response('Not Found', { status: 404 });
-            }
-        })
+  get(path: string, handler: RouteHandler) {
+    this.router.addRoute('GET', path, handler);
+    return this;
+  }
 
-        console.log(`Server running on http://localhost:${port}`);
-    }
-    
+  //Running server
+  serve(port: number) {
+    //Bun server
+    Bun.serve({
+      port,
+      fetch: (req) => {
+        const url = new URL(req.url);
+        const route = this.router.findRoute(
+          req.method as HttpMethodType,
+          url.pathname
+        );
+        console.log('reached in serve: ', route);
+
+        //If router is found then
+        if (route) {
+          const ctx = this.createContext(req);
+
+          return route.handler(ctx);
+        }
+        return new Response('Not Found', { status: 404 });
+      },
+    });
+
+    console.log(`Server running on http://localhost:${port}`);
+  }
 }
