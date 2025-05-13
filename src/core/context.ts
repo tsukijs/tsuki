dcceb1eacfd39feff7f08a233e30697363118d7e
Saif Ali <this.saif.email@gmail.com>
2025-05-13 08:50:14 +0530

    feat(context): add text() and json() response helpers

--- src/core/context.ts
+++ src/core/context.ts
@@ -3,4 +3,8 @@ export interface Context {
     req: Request;
     params: Record<string, string>;
     body?: any;
+
+    text: (body:string, status?: number) => Response;
+    json: (body:any, status?: number) => Response;
+
 }
