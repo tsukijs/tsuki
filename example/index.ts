402b512e31dff18139a7073532b3a7df9747be98
Saif Ali <this.saif.email@gmail.com>
2025-06-03 01:53:09 +0530

    example of trie router

--- example/index.ts
+++ example/index.ts
@@ -1,33 +1,20 @@
-import { z } from 'zod';
-import { Tsuki } from '../src/core/tsuki';
-import type { Context } from '../src/core/context';
+import { Tsuki } from '../src/tsuki';
 
-const userSchema = z.object({
-  name: z.string(),
-  age: z.number(),
-  email: z.string().email(),
-  username: z.string().min(3).max(20),
-})
-
-const loginSchema = z.object({
-  username: z.string().min(3).max(20),
-  password: z.string().min(8).max(20),
-})
-
-const loginHandler = (c:Context<z.infer<typeof loginSchema>>) => {
-  return c.json({ message: 'Login successful!', data: c.body });
-}
 
 const app = new Tsuki();
 app.get('/hello', () => new Response('Hello World!'));
 app.get('/json', (c) => c.json({ message: 'Hello World!' }));
 app.get('/text', (c) => c.text('this is a text response'));
-app.post('/user', userSchema, (c) => {
-    console.log('user data: ', c.body);
-    
-    return c.json({ message: 'User created successfully!', data: c.body });
+app.get('/pokemon/:name', (c) => {
+  console.log('name:', c.params.name);
+  
+  return c.json({ message: `Pokemon name: ${c.params.name}` });
 })
 
-app.post('/login', loginSchema, loginHandler);
+
+// GET /user/:id
+app.get('/user/:name/likes/:food', (c) => {
+  return c.json({ message: {name: c.param('name'), food: c.param('food')} });
+});
 
 app.serve(3000);
